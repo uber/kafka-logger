cb2bc8d7b861371f7798c3a0ac723b77e1f8621d
Hongliang Xu <hongxu@uber.com>
2016-06-13 21:25:27 -0700

    add parentheses for ||

--- index.js
+++ index.js
@@ -199,7 +199,7 @@ KafkaLogger.prototype.log = function(level, msg, meta, callback) {
     logMessage.msg = msg;
     logMessage.fields = meta;
 
-    if ((this.kafkaClient && !this.connected) || (this.kafkaRestClient && !this.kafkaRestClientConnected) && Date.now() < this.initTime + 5000) {
+    if (((this.kafkaClient && !this.connected) || (this.kafkaRestClient && !this.kafkaRestClientConnected)) && Date.now() < this.initTime + 5000) {
         return this.initQueue.push([logMessage, callback]);
     } else if (this.connected && this.initQueue.length) {
         this._flush();
